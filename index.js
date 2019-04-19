18be38a858826af24870f49f65b9a084b4f557b0
amber <mnwan@qq.com>
2019-04-19 22:25:15 +0800

    使用vue和layer

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        data: [
+            {msg:'one'},
+            {msg:'two'},
+            {msg:'three'},
+            {msg:'four'},
+        ]
+    }
+});
---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+var nav= new Vue({
+    el: '#nav',
+    data: {
+        names: [
+            { name: '学习 JavaScript' },
+            { name: '学习 Vue' },
+            { name: '整个牛项目' }
+        ]
+    }
+})
