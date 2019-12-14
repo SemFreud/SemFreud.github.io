80ad9dd6d9f621d31e18ac458da7151ac37ca2ee
amber <mnwan@qq.com>
2019-12-14 19:30:54 +0800

    增加---

--- static/js/index.js
+++ static/js/index.js
@@ -2,24 +2,26 @@ var nav = new Vue({
     el: '#global',
     data: {
         nav_list: '',
-        md_content: ''
+        md_content: '',
+        curr_file_info: {
+            name: '',
+            date: ''
+        }
     },
     methods: {
-        getFile: function (md_file_url) {
-            // console.log(md_file_url)
-            this.$http.get(md_file_url).then(function (res) {
-                // console.log(res.body)
-                // this.md_content = res.body
+        getFile: function (item) {
+            this.$http.get(item.url).then(function (res) {
                 var converter = new showdown.Converter({extensions: ['table']})
                 var html = converter.makeHtml(res.body);
-                console.log(html)
                 document.getElementById("md_content").innerHTML = html;
+                this.curr_file_info.date = item.date
+                this.curr_file_info.name = item.name
             })
-        },
+        }
     },
     created: function () {
         this.$http.get("static/data/index_bar.json").then(function (res) {
-            this.nav_list = res.body.name_list
+            this.nav_list = res.body
         })
     }
-})
+});
